0fa018feb235f7be3048b09df4ba121f99eea471
leobema <leonardoberti21@gmail.com>
2024-02-14 13:10:34 -0300

    Agregar Mejoras

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   inputText = '';
-  
+
+  constructor(){
+      
+  }
+
 
   button(btn: String): void {
     if (btn === 'Del') {
@@ -23,8 +28,60 @@ export class AppComponent {
   }
 
   resultado(btn: String) {
-    if (btn === '=') {
-    this.inputText = (this.resultado, eval(this.inputText))
+    if (btn === '=')
+    debugger
+    {
+    this.inputText = (this.resultado, eval(this.inputText)); 
     }
   } 
+
+  consition2(){
+    if (
+      this.inputText === '*' ||
+      this.inputText === '+' ||
+      this.inputText === '-' ||
+      this.inputText === '.' ||
+      this.inputText === '/' 
+      ){
+    this.inputText = ''; 
+  }
+   }
+
+   condition(){
+    const hola = this.inputText.slice(this.inputText.length -2)
+    if ( 
+      hola === '**' || 
+      hola === '++' || 
+      hola === '--' || 
+      hola === '..' || 
+      hola === '//' ||
+      hola === '.*' ||
+      hola === './' ||
+      hola === '.+' ||
+      hola === '.-' ||
+      hola === '*.' ||
+      hola === '*+' ||
+      hola === '*-' ||
+      hola === '*/' ||
+      hola === '+.' ||
+      hola === '+/' ||
+      hola === '+-' ||
+      hola === '-.' ||
+      hola === '-*' ||
+      hola === '-+' || 
+      hola === '-/' || 
+      hola === './' ||
+      hola === '+*' ||
+      hola === '/.' ||
+      hola === '/+' ||
+      hola === '/-' ||
+      hola === '/*'
+      ){
+    this.button('Del');
+    } 
+  } 
+
+     
+  
+
 }
